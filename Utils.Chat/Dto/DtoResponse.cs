834dcd28da96585683e6d21bd5da0768caea7b47
IgnacioHerreria <iherreria918@live.com>
2020-05-03 11:29:52 -0500

    dto utils

---
+++ Utils.Chat/Dto/DtoResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Utils.Chat.Dto
+{
+    public class DtoResponse
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("dt1")]
+        public string Dt1 { get; set; }
+        [JsonProperty("dt2")]
+        public string Dt2 { get; set; }
+        [JsonProperty("dt3")]
+        public string Dt3 { get; set; }
+        [JsonProperty("dt4")]
+        public string Dt4 { get; set; }
+    }
+}
